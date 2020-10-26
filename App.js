219cba53fe8209bd2b35b1dad87bfb392c10e5e0
vnavarro <vnavarro@brightcove.com>
2020-10-26 14:26:29 -0600

    Initial commit

--- App.js
+++ App.js
@@ -1,17 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, Modal, StyleSheet, Text, View } from 'react-native';
+
+import { Map } from './components';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Map/>
+      <Modal
+        animationType='slide'
+        transparent={true}
+        visible={true}
+      >
+        <View style={styles.center}>
+          <View style={styles.modalView}>
+            <Text>😃</Text>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalView: {
+    backgroundColor: '#fff',
+    borderRadius: 4,
+    padding: 20,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+  }, 
   container: {
     flex: 1,
     backgroundColor: '#fff',
---
+++ components/Map.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, Dimensions } from 'react-native';
+
+export default () => {
+	return (
+		<MapView style={styles.map} />
+	);
+}
+
+const styles = StyleSheet.create({
+	map: {
+		height: Dimensions.get('window').height,
+		width: Dimensions.get('window').width,
+	},
+})
+
+
+
