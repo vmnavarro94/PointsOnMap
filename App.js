72e657a63b66f614ad55377af1eebe4e57ddf7d3
vnavarro <vnavarro@brightcove.com>
2020-10-27 08:56:33 -0600

    Add points to state

--- App.js
+++ App.js
@@ -1,23 +1,38 @@
 import React, { useState } from 'react';
-import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, View, Button } from 'react-native';
 
-import { Map, Modal, Panel } from './components';
+import { Map, Modal, Panel, Input } from './components';
 
 export default function App() {
   const [puntos, setPuntos] = useState([]);
-
+  const [nombre, setNombre] = useState('');
+  const [puntoTemp, setPuntoTemp] = useState({});
+  const [showModal, setShowModal] = useState(false);
   const handleLongPress = ({ nativeEvent }) => {
-    setPuntos([...puntos, {coordinate: nativeEvent.coordinate}]);
+    setPuntoTemp(nativeEvent.coordinate);
+    setShowModal(true);
   };
+
+  const handleChangeText = text => {
+    setNombre(text);
+  }
+
+  const handleSubmit = () => {
+    const newPunto = { coordinate: puntoTemp, name: nombre };
+    setPuntos([...puntos, newPunto]);
+    setShowModal(false);
+    setNombre('');
+  }
 
   console.log(puntos);
   return (
     <View style={styles.container}>
       <Map longPress={handleLongPress}/>
-      <Modal 
-        showModal={true}
+      <Modal showModal={showModal}
       >
-        <Text>Ejemplirijillo</Text>
+        <Input title='Nombre' placeholder='Nombre del punto' onChangeText={handleChangeText} />
+        <Button title='Aceptar' onPress={handleSubmit}/>
+        <Button title='Cancelar' onPress={() => {}}/>
       </Modal>
       <Panel />
     </View>
